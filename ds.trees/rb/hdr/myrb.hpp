5a8708c8fe076544f8fa77e4a50792e91ebd721b
cnorden <git@gitlab.eecs.wsu.edu>
2020-10-28 13:40:53 -0700

    minor format change

--- ds.trees/rb/hdr/myrb.hpp
+++ ds.trees/rb/hdr/myrb.hpp
@@ -1,9 +1,10 @@
 #ifndef MYRB_HPP
 #define MYRB_HPP
 
+#include <initializer_list>
 #include <iostream>
 
-enum Color { RED = 0, BLACK = 1 };
+enum Paint { RED = 0, BLACK = 1 };
 
 template <typename T>
 class RbNode {
@@ -13,7 +14,7 @@ class RbNode {
   RbNode* left;
   RbNode* right;
   RbNode* parent;
-  Color color = BLACK;
+  Paint color = BLACK;
 
  public:
   /***************************PUBLIC API***************************/
@@ -31,61 +32,151 @@ class RbNode {
   RbNode*& GetL() { return this->left; }
   RbNode*& GetR() { return this->right; }
   RbNode*& GetP() { return this->parent; }
-  Color GetColor() { return this->color; }
+  int GetColor() { return this->color; }
   // Setters:
   void setData(T dt) { this->data = new T{dt}; }
   RbNode*& setLeft(RbNode*& n) { return this->left = n; }
   RbNode*& setRight(RbNode*& n) { return this->right = n; }
   RbNode*& setParent(RbNode*& n) { return this->parent = n; }
-  void setColor(Color c) { this->color = c; }
+  void setColor(Paint c) { this->color = c; }
+  void setRed() { this->color = RED; }
+  void setBlack() { this->color = BLACK; }
 };
 
 template <typename T>
 class RbT {
  private:
   /*****************************PRIVATE****************************/
-  RbNode<T>* _root;
+  RbNode<T>* _Root;
 
-  // Destruct the subtree, starting from the supplied node:
-  // NOTE: Might be more time/space efficient if use a loop for large trees:
-  bool _isEmptyTree() { return (!_root) ? true : false; }
-  void _destructFrom(RbNode<T>*& r) {
-    if (r) {
-      _destructFrom(r->left);
-      _destructFrom(r->right);
-      delete r;
+  // Check if Node is empty:
+  bool _isEmptyAt(RbNode<T>*& n) { return (!n) ? true : false; }
+  // Destruct SubTree with root at given node:
+  bool _destructFrom(RbNode<T>*& n) {
+    if (n) {
+      _destructFrom(n->GetL());
+      _destructFrom(n->GetR());
+      delete n;
+      return true;
     }
+    // NOTE: More time/space efficient for large trees with loop?
+    else
+      return false;
   }
 
   void _rotateLeft(RbNode<T>*& n) {}
   void _rotateRight(RbNode<T>*& n) {}
 
-  void _insert(RbNode<T>*& n, T val) {
+  void _insertFrom(RbNode<T>*& n, T val) {
     if (!n) {
       n = new RbNode<T>{val};
-      if (n->GetP()->GetColor() == RED) {
-        n->setColor(BLACK);
-      } else {
-        n->setColor(RED);
-      }
+      // if (n->GetP()->GetColor() == RED) {
+      //   n->setBlack();
+      // } else {
+      //   n->setRed();
+      // }
     } else {
       if (n->getData() > val) {
-        _insert(n->GetL(), val);
+        _insertFrom(n->GetL(), val);
       } else if (n->getData() < val) {
-        _insert(n->GetR(), val);
+        _insertFrom(n->GetR(), val);
       } else
         return;
+    }
+  }
+  void _printIN(RbNode<T>*& n, std::ostream& os) {
+    if (!n) return;
+    _printIN(n->GetL(), os);
+    os << n->getData() << ' ';
+    _printIN(n->GetR(), os);
+  }
+
+  void _printPRE(RbNode<T>*& n, std::ostream& os) {
+    if (!n) return;
+    os << n->getData() << ' ';
+    _printPRE(n->GetL(), os);
+    _printPRE(n->GetR(), os);
+  }
+
+  void _printPOST(RbNode<T>*& n, std::ostream& os) {
+    if (!n) return;
+    _printPOST(n->GetL(), os);
+    _printPOST(n->GetR(), os);
+    os << n->getData() << ' ';
+  }
+
+  bool b_printIN(RbNode<T>*& n, std::ostream& os) {
+    if (!n)
+      return false;
+    else {
+      b_printIN(n->GetL(), os);
+      os << n->getData() << ' ';
+      b_printIN(n->GetR(), os);
+      return true;
+    }
+  }
+
+  bool b_printPRE(RbNode<T>*& n, std::ostream& os) {
+    if (!n)
+      return false;
+    else {
+      os << n->getData() << ' ';
+      b_printPRE(n->GetL(), os);
+      b_printPRE(n->GetR(), os);
+      return true;
+    }
+  }
+
+  bool b_printPOST(RbNode<T>*& n, std::ostream& os) {
+    if (!n)
+      return false;
+    else {
+      b_printPOST(n->GetL(), os);
+      b_printPOST(n->GetR(), os);
+      os << n->getData() << ' ';
+      return true;
     }
   }
 
  public:
   /***************************PUBLIC API***************************/
   // Constructor & Destructor:
-  RbT() : _root{nullptr} {}
-  ~RbT() { _destructFrom(_root); }
+  RbT() : _Root{nullptr} {}
+  RbT(std::initializer_list<T>& list) {
+    _Root = nullptr;
+    for (auto i : list)
+      insert(i);
+  }
+
+  ~RbT() { _destructFrom(_Root); }
 
   // Operations on the tree:
-  bool isEmptyTree() { return _isEmptyTree(); }
+  bool isEmptyTree() { return _isEmptyAt(_Root); }  // validate the tree?
+  bool clearTree() { return _destructFrom(_Root); }
+
+  void insert(T& val) {
+    // immediately create node if _Root is empty:
+    if (!_Root) {
+      _Root = new RbNode<T>{val};
+      _Root->setBlack();
+    }
+    // otherwise, start insertion from _Root:
+    else
+      _insertFrom(_Root, val);
+  }
+  void print_InOrder(std::ostream& os = std::cout) { _printIN(_Root, os); }
+  void print_PreOrder(std::ostream& os = std::cout) { _printPRE(_Root, os); }
+  void print_PostOrder(std::ostream& os = std::cout) { _printPOST(_Root, os); }
+
+  bool bprint_InOrder(std::ostream& os = std::cout) {
+    return b_printIN(_Root, os);
+  }
+  bool bprint_PreOrder(std::ostream& os = std::cout) {
+    return b_printPRE(_Root, os);
+  }
+  bool bprint_PostOrder(std::ostream& os = std::cout) {
+    return b_printPOST(_Root, os);
+  }
 };
 
 #endif
--- ds.trees/rb/src/unitTest.cpp
+++ ds.trees/rb/src/unitTest.cpp
@@ -4,59 +4,101 @@ bool unitTestFunc() {
   /* DECLARE A PASSMARK FLAG */
   static bool allPassed = true;
   static std::string thisTest = "";
+  static int test_id = 0;
 
-  /* INIT TEST: OUTPUT STRING */
+  /*******************************************************************************/
   thisTest = "INIT OUTPUT";
-  std::cout << "\n\n";
-  std::cout << "TEST #0: " << thisTest << "\n";
+  TestHeader(test_id, thisTest);
+  /*******************************************************************************/
   allPassed = []() {
-    std::string tString = "\tTHIS IS A TEST STRING.";
-    std::cout << tString << std::endl;
+    std::string tString = "THIS IS A TEST STRING.";
+    std::cout << tString;
     return true;
   }();
   errCatch(allPassed, thisTest);
 
-  /* TEST: CONSTRUCT A RbNode */
+  /*******************************************************************************/
   thisTest = "CONSTRUCT A RbNode{5}";
-  std::cout << "\n\n";
-  std::cout << "TEST #1: " << thisTest << "\n";
+  ++test_id;
+  TestHeader(test_id, thisTest);
+  /*******************************************************************************/
   int init_val = 5;
   RbNode<int> nodeA{init_val};
   allPassed = [&]() {
     allPassed = (nodeA.GetColor() == BLACK) ? true : false;
-    std::cout << "\tDEFAULT BLACK = 1, COLOR: " << nodeA.GetColor()
-              << std::endl;
+    std::cout << "DEFAULT BLACK = 1, COLOR: " << nodeA.GetColor() << '\n';
     allPassed = (nodeA.getData() == 5) ? true : false;
-    std::cout << "\tCorrect Init:" << init_val
-              << ", returned=" << nodeA.getData() << std::endl;
+    std::cout << "Correct Init:" << init_val
+              << ", returned=" << nodeA.getData();
     return allPassed;
   }();
   errCatch(allPassed, thisTest);
 
-  /* TEST: CONSTRUCT A RbT */
-  thisTest = "CONSTRUCT A RB TREE";
-  std::cout << "\n\n";
-  std::cout << "TEST #2: " << thisTest << "\n\t";
+  /*******************************************************************************/
+  thisTest = "MAKE TREE W/ {1, 6, 8, 13, 19, 5, 2, 7}";
+  ++test_id;
+  TestHeader(test_id, thisTest);
+  /*******************************************************************************/
+  std::initializer_list<int> list = {1, 6, 8, 13, 19, 5, 2, 7};
+  RbT<int> newTree{list};
+  allPassed = newTree.bprint_InOrder();
+  errCatch(allPassed = true, thisTest);
 
-  /* TEST: */
-  thisTest = "CONSTRUCT A RB TREE";
-  std::cout << "\n\n";
-  std::cout << "TEST #2: " << thisTest << "\n\t";
+  /*******************************************************************************/
+  thisTest = "TRAVERSING & PRINTING IN ORDER";
+  ++test_id;
+  TestHeader(test_id, thisTest);
+  /*******************************************************************************/
+  newTree.print_InOrder();
+  std::cout << '\n';
+  allPassed = newTree.bprint_InOrder();
+  errCatch(allPassed, thisTest);
 
-  /* TEST: */
-  thisTest = "CONSTRUCT A RB TREE";
-  std::cout << "\n\n";
-  std::cout << "TEST #2: " << thisTest << "\n\t";
+  /*******************************************************************************/
+  thisTest = "TRAVERSING & PRINTING PRE ORDER";
+  ++test_id;
+  TestHeader(test_id, thisTest);
+  /*******************************************************************************/
+  newTree.print_PreOrder();
+  std::cout << '\n';
+  allPassed = newTree.bprint_PreOrder();
+  errCatch(allPassed, thisTest);
+
+  /*******************************************************************************/
+  thisTest = "TRAVERSING & PRINTING POST ORDER";
+  ++test_id;
+  TestHeader(test_id, thisTest);
+  /*******************************************************************************/
+  newTree.print_PostOrder();
+  std::cout << '\n';
+  allPassed = newTree.bprint_PostOrder();
+  errCatch(allPassed, thisTest);
+
+  /*******************************************************************************/
+  thisTest = "DESTRUCT A TREE";
+  ++test_id;
+  TestHeader(test_id, thisTest);
+  /*******************************************************************************/
+  newTree.clearTree();
+  allPassed = [&]() {
+    if (!newTree.isEmptyTree()) {
+      std::cout << "Tree is empty." << std::endl;
+      return true;
+    } else {
+      std::cout << "Tree NOT empty." << std::endl;
+      return false;
+    }
+  }();
 
   return allPassed;
 }
 
 void pfCheck(bool check, std::string testName) {
-  std::cout << ">>>>> " << testName << ": ";
+  std::cout << "\n\n>>>>> " << testName << "\n";
   if (check)
-    std::cout << "PASSED." << std::endl;
+    std::cout << "STATUS: PASSED." << std::endl;
   else
-    throw "\nERROR: FAILED PASSCHECK ON TEST: " + testName;
+    throw "\nERROR! FAILED PASSCHECK ON TEST: " + testName;
 }
 
 void errCatch(bool check, std::string testName) {
@@ -65,4 +107,11 @@ void errCatch(bool check, std::string testName) {
   } catch (std::string msg) {
     std::cout << msg << std::endl;
   }
+}
+
+void TestHeader(int test_id, std::string thisTest) {
+  std::cout << "\n\n";
+  std::cout << "TEST #" << test_id << ": " << thisTest << "\n";
+  std::cout
+      << "----------------------------------------------------------------\n";
 }
